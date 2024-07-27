ecbd13c2720cdc4839d3fc69be8ebf6734d87e87
tanvivekhande1234 <tanvivekhande@gmail.com>
2024-07-28 00:45:34 +0530

    Hello1 and Hello2 is created

--- Hello1.java
+++ Hello1.java
@@ -1,6 +1,6 @@
 public class Hello {
                    
-                public stativ void main(String[] args) {
+                public static void main(String[] args) {
                                    system.out.printl("Hello-1");
 
                 }
