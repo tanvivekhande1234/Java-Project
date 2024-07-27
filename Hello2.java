8189b882ddc9a5783fa5f046346172c6559f21c5
tanvivekhande1234 <tanvivekhande@gmail.com>
2024-07-28 00:28:48 +0530

    Hello1 and Hello2 is created

---
+++ Hello2.java
@@ -0,0 +1,8 @@
+public class Hello {
+                   
+                public stativ void main(String[] args) {
+                                   system.out.printl("Hello-1");
+
+                }
+
+}
